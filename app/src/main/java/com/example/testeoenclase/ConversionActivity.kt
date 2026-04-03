eff07ec06e76925896d4d0d2d82e3c0bd9381c01
Alexander1909 <jhonathan.corrales@ucsm.edu.pe>
2026-04-03 10:56:42 -0500

    update ConversionActivity.kt

--- app/src/main/java/com/example/testeoenclase/ConversionActivity.kt
+++ app/src/main/java/com/example/testeoenclase/ConversionActivity.kt
@@ -13,3 +13,38 @@ class ConversionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_conversion)
+        val etMonto = findViewById<EditText>(R.id.etMonto)
+        val btnConvertir = findViewById<Button>(R.id.btnConvertir)
+        val tvResultado = findViewById<TextView>(R.id.tvResultado)
+        progressBar = findViewById(R.id.progressBar)
+
+        btnConvertir.setOnClickListener {
+            val monto = etMonto.text.toString().toDoubleOrNull()
+
+            if (monto != null) {
+                val resultado = monto * 0.27  // Ejemplo Soles a USD
+                tvResultado.text = "USD: $resultado"
+                iniciarCarga()
+            } else {
+                Toast.makeText(this, "Ingrese un valor válido", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun iniciarCarga() {
+        progreso = 0
+        val handler = Handler()
+
+        Thread {
+            while (progreso < 100) {
+                progreso += 10
+                Thread.sleep(200)
+
+                handler.post {
+                    progressBar.progress = progreso
+                }
+            }
+            Log.d("Carga", "Carga completa")
+        }.start()
+    }
+}
