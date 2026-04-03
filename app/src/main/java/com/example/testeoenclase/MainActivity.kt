cea54579d6945f3f08aa4ddeffd857b3d98b735c
Alexander1909 <jhonathan.corrales@ucsm.edu.pe>
2026-04-03 10:11:41 -0500

    Initial commit

---
+++ app/src/main/java/com/example/testeoenclase/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.testeoenclase
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.testeoenclase.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Inicializar ViewBinding correctamente
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        logLifecycle("onCreate")
+
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        binding.btnMostrar.setOnClickListener {
+            startActivity(Intent(this, SegundaActividad::class.java))
+        }
+    }
+
+    private fun logLifecycle(event: String) {
+        Log.d(TAG, "Evento: $event")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        logLifecycle("onStart")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        logLifecycle("onRestart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        logLifecycle("onResume")
+    }
+
+    override fun onPause() {
+        logLifecycle("onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        logLifecycle("onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        logLifecycle("onDestroy")
+        super.onDestroy()
+    }
+}
